2a8471ea2c582b300329c0b5fed70b1fd92f25cb
Lombardo Paredes <sglpared@liverpool.ac.uk>
2021-11-11 00:50:02 +0000

    Refactor

--- database/download.go
+++ database/download.go
@@ -28,92 +28,114 @@ func StartDownloadChannel(ch chan api.DownloadRequest, dbConn string, client *ap
 }
 
 func (c *Connection) RequestDownloadFiles(path string, client *api.CopernicusClient, downloadChan chan<- api.DownloadRequest, allowRequest bool) {
-	query := "SELECT uuid, title, online, requestedDate, priority, downloaded FROM file WHERE lockedBy IS NULL AND downloaded = FALSE AND (requestedDate IS NULL OR checkedDate < DATE_SUB(NOW(), INTERVAL 10 MINUTE) OR online = TRUE) ORDER BY priority DESC, online DESC, checkedDate ASC, RAND() DESC LIMIT 10;"
+	toDownloadQuery := "SELECT uuid, title, online, requestedDate, checkedDate, priority, downloaded FROM file WHERE lockedBy IS NULL AND downloaded = FALSE AND online = TRUE ORDER BY priority DESC, online DESC, checkedDate DESC, RAND() DESC LIMIT 1;"
+	toCheckQuery := "SELECT uuid, title, online, requestedDate, checkedDate, priority, downloaded FROM file WHERE requestedDate IS NOT NULL AND downloaded = FALSE AND checkedDate < DATE_SUB(NOW(), INTERVAL 10 MINUTE) ORDER BY priority DESC, online DESC, checkedDate DESC, RAND() DESC LIMIT 10;"
+	toRequestQuery := "SELECT uuid, title, online, requestedDate, checkedDate, priority, downloaded FROM file WHERE requestedDate IS NULL AND downloaded = FALSE ORDER BY priority DESC, online DESC, checkedDate DESC, RAND() DESC LIMIT 10;"
 
-	results, err, _ := c.Query(query)
+	toDownload, err, _ := c.Query(toDownloadQuery)
 	if err != nil {
 		log.Fatalf("Error while querying database: %s", err.Error())
 	}
 
-	if !allowRequest {
-		client.RequestQuotaExceeded = true
+	toRequest, err, _ := c.Query(toRequestQuery)
+	if err != nil {
+		log.Fatalf("Error while querying database: %s", err.Error())
 	}
 
-	if client.RequestQuotaExceeded {
-		onlineFilesQuery, _, _ := c.Query("SELECT COUNT(uuid) count FROM file WHERE online = TRUE AND downloaded = FALSE AND lockedBy IS NULL")
-		noOnlineFiles := string(onlineFilesQuery[0]["count"].([]byte)) == "0"
+	toCheck, err, _ := c.Query(toCheckQuery)
+	if err != nil {
+		log.Fatalf("Error while querying database: %s", err.Error())
+	}
 
-		requestedFilesQuery, _, _ := c.Query("SELECT COUNT(uuid) count FROM file WHERE online = FALSE AND lockedBy IS NULL AND requestedDate IS NOT NULL AND checkedDate < DATE_SUB(NOW(), INTERVAL 10 MINUTE)")
-		noRequestedFiles := string(requestedFilesQuery[0]["count"].([]byte)) == "0"
+	noToCheck := len(toCheck) == 0
+	noToDownload := len(toDownload) == 0
 
-		if noOnlineFiles && noRequestedFiles {
-			log.Println("There are no online files to request download and the quota is exceeded, sleeping for 10 minutes.")
-			time.Sleep(10 * time.Minute)
+	if noToCheck && noToDownload && (client.RequestQuotaExceeded || !allowRequest) {
+		log.Println("There are no online files to check or download, and the quota is exceeded. Sleeping for 10 minutes.")
+		time.Sleep(10 * time.Minute)
+		return
+	}
+
+	for _, row := range toDownload {
+		c.ProcessDownload(row, path, client, downloadChan)
+	}
+
+	for _, row := range toCheck {
+		c.ProcessCheck(row, client)
+	}
+
+	if allowRequest {
+		for _, row := range toRequest {
+			c.ProcessRequest(row, client)
 		}
 	}
 
-	for _, row := range results {
-		c.ProcessDownload(row, path, client, downloadChan, allowRequest)
-	}
 }
 
-func (c *Connection) ProcessDownload(row map[string]interface{}, path string, client *api.CopernicusClient, downloadChan chan<- api.DownloadRequest, allowRequest bool) {
+func (c *Connection) ProcessDownload(row map[string]interface{}, path string, client *api.CopernicusClient, downloadChan chan<- api.DownloadRequest) {
 
 	uuid := string(row["uuid"].([]byte))
 	title := string(row["title"].([]byte))
-	online := string(row["online"].([]byte)) == "1"
-	requestedDateStr := row["requestedDate"]
+
+	log.Println(fmt.Sprintf("File %s is online. Requesting its download", title))
+	c.LockByUUID(uuid, client)
+	downloadChan <- api.DownloadRequest{Title: title, Path: path, Uuid: uuid}
+}
+
+func (c *Connection) ProcessCheck(row map[string]interface{}, client *api.CopernicusClient) {
+
+	uuid := string(row["uuid"].([]byte))
+	title := string(row["title"].([]byte))
 	checkedDateStr := row["checkedDate"]
 
-	if online {
-		log.Println(fmt.Sprintf("File %s is online. Requesting its download", title))
-		c.LockByUUID(uuid, client)
-		downloadChan <- api.DownloadRequest{Title: title, Path: path, Uuid: uuid}
-	} else if allowRequest && requestedDateStr == nil {
-		// Do not request new files if we know the quota is exceeded
-		if client.RequestQuotaExceeded && !client.RequestQuotaExceededAt.After(time.Now().Add(-time.Minute*10)) {
-			client.RequestQuotaExceeded = false
-		} else if client.RequestQuotaExceeded {
-			return
-		}
+	log.Println(fmt.Sprintf("Checking if requested file %s is online", title))
+	entry := c.UpdateByUUID(uuid, client)
 
-		log.Println(fmt.Sprintf("File %s is not online. Requesting it", title))
-
-		err := client.Request(uuid)
-		if err != nil {
-			switch err.(type) {
-			case api.QuotaExceededError:
-				log.Println(err.Error())
-				client.RequestQuotaExceeded = true
-				client.RequestQuotaExceededAt = time.Now()
-			case api.CantRequestOnlineError:
-				//File is actually online, update it
-				c.UpdateByUUID(uuid, client)
-			case api.TooManyRequestsError:
-				log.Println(err.Error())
-				time.Sleep(time.Second * 10)
-			case api.OtherError:
-				log.Println(err.Error())
-			}
-			return
-		}
-		c.SetRequested(uuid)
+	if entry.Properties.Online {
+		c.Execute("UPDATE file SET priority = ? WHERE uuid = ?", 3, uuid)
 	} else {
-		log.Println(fmt.Sprintf("Checking if requested file %s is online", title))
-		entry := c.UpdateByUUID(uuid, client)
-
-		if entry.Properties.Online {
-			c.Execute("UPDATE file SET priority = ? WHERE uuid = ?", 3, uuid)
-		} else {
-			if checkedDateStr != nil {
-				checkedDate, _ := time.Parse("2006-01-02 15:04:05", string(checkedDateStr.([]byte)))
-				// If the file has not been uploaded in 3 hours, delete the request
-				if checkedDate.After(time.Now().Add(-time.Hour * 3)) {
-					log.Println(fmt.Sprintf("More than 3 hours since request off %s, setting it up for requesting again", title))
-					c.Execute("UPDATE file SET requestedDate = ?, checkedDate = ? WHERE uuid = ?", nil, nil, uuid)
-				}
+		if checkedDateStr != nil {
+			checkedDate, _ := time.Parse("2006-01-02 15:04:05", string(checkedDateStr.([]byte)))
+			// If the file has not been uploaded in 3 hours, delete the request
+			if checkedDate.After(time.Now().Add(-time.Hour * 3)) {
+				log.Println(fmt.Sprintf("More than 3 hours since request off %s, setting it up for requesting again", title))
+				c.Execute("UPDATE file SET requestedDate = ?, checkedDate = ? WHERE uuid = ?", nil, nil, uuid)
 			}
 		}
-		c.SetChecked(uuid)
 	}
+	c.SetChecked(uuid)
+}
+
+func (c *Connection) ProcessRequest(row map[string]interface{}, client *api.CopernicusClient) {
+	uuid := string(row["uuid"].([]byte))
+	title := string(row["title"].([]byte))
+
+	// Do not request new files if we know the quota is exceeded
+	if client.RequestQuotaExceeded && !client.RequestQuotaExceededAt.After(time.Now().Add(-time.Minute*10)) {
+		client.RequestQuotaExceeded = false
+	} else if client.RequestQuotaExceeded {
+		return
+	}
+
+	log.Println(fmt.Sprintf("File %s is not online. Requesting it", title))
+
+	err := client.Request(uuid)
+	if err != nil {
+		switch err.(type) {
+		case api.QuotaExceededError:
+			log.Println(err.Error())
+			client.RequestQuotaExceeded = true
+			client.RequestQuotaExceededAt = time.Now()
+		case api.CantRequestOnlineError:
+			//File is actually online, update it
+			c.UpdateByUUID(uuid, client)
+		case api.TooManyRequestsError:
+			log.Println(err.Error())
+			time.Sleep(time.Second * 10)
+		case api.OtherError:
+			log.Println(err.Error())
+		}
+		return
+	}
+	c.SetRequested(uuid)
 }
